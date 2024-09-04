6026f88445fb2c20d9b5bbb87002ded83f33ff93
Diego Hernández <dahernandezsilvestre@gmail.com>
2024-09-03 19:59:39 -0600

    error

--- Client/router.py
+++ Client/router.py
@@ -54,8 +54,8 @@ if __name__ == "__main__":
 
     if sys.argv[1] == 'true':
         threads = []
-        threads.append(threading.Thread(target=asyncNode, args=('B', 'ines130602', sys.argv[2], '../config/names2024-ConjuntoA-2024.txt', '../config/topo2024-ConjuntoA-2024.txt')))
-        threads.append(threading.Thread(target=asyncNode, args=('C', '123', sys.argv[2], '../config/names2024-ConjuntoA-2024.txt', '../config/topo2024-ConjuntoA-2024.txt')))
+        threads.append(threading.Thread(target=asyncNode, args=('B', 'ines130602', sys.argv[2], '../config/names2024-Conjunto1-9-4-2024.txt', '../config/topo2024-Conjunto1-9-4-2024.txt')))
+        threads.append(threading.Thread(target=asyncNode, args=('C', '123', sys.argv[2], '../config/names2024-Conjunto1-9-4-2024.txt', '../config/topo2024-Conjunto1-9-4-2024.txt')))
         #threads.append(threading.Thread(target=asyncNode, args=('A', '1234', sys.argv[2], '../config/names2024-randomX-2024.txt', '../config/topo2024-randomX-2024.txt')))
         #threads.append(threading.Thread(target=asyncNode, args=('E', '1234', sys.argv[2], '../config/names2024-randomX-2024.txt', '../config/topo2024-randomX-2024.txt')))
 
@@ -69,4 +69,4 @@ if __name__ == "__main__":
         node_id = sys.argv[1]
         password = sys.argv[2]
         routing_algorithm = sys.argv[3]    
-        asyncio.run(startNode(node_id, password, routing_algorithm, '../config/names2024-ConjuntoA-2024.txt', '../config/topo2024-ConjuntoA-2024.txt', True))
+        asyncio.run(startNode(node_id, password, routing_algorithm, '../config/names2024-Conjunto1-9-4-2024.txt', '../config/topo2024-Conjunto1-9-4-2024.txt', True))
