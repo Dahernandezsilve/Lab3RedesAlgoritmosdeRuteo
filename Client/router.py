95e04fee5471120d82c5ea81f2b224c0dde8cb4b
auyjos <josea13p@gmail.com>
2024-09-04 19:35:35 -0600

    Implementación de input y ttinker

--- Client/router.py
+++ Client/router.py
@@ -5,8 +5,26 @@ from communicationManager import CommunicationManager
 from MessageHandler import MessageHandler
 import sys
 import threading
+import tkinter as tk
+from tkinter import simpledialog
 
-async def startNode(node_id: str, password: str, routing_algorithm: str, type_names: str, type_topo: str, sendMessage= False):
+
+# Función para mostrar una ventana de entrada con tkinter
+def ask_for_message():
+    root = tk.Tk()
+    root.withdraw()  # Oculta la ventana principal
+
+    recipient = simpledialog.askstring(
+        "Recipient", "Escribe la dirección del destinatario (e.g., user@server.lol):")
+    message = simpledialog.askstring(
+        "Message", "Escribe el mensaje que deseas enviar:")
+
+    root.destroy()  # Cierra la ventana
+
+    return recipient, message
+
+
+async def startNode(node_id: str, password: str, routing_algorithm: str, type_names: str, type_topo: str, sendMessage=False):
     server = 'alumchat.lol'
     port = 5222
 
@@ -15,30 +33,37 @@ async def startNode(node_id: str, password: str, routing_algorithm: str, type_na
 
     account_manager = AccountManager(server, port)
     username = config.get_node_info()['address']
-    password = password 
+    password = password
     user = username.split('@')[0]
 
     try:
-        account_manager.login(user, password)        
-        comm_manager = CommunicationManager(account_manager.client, nodeConfig=config, routing_algorithm=routing_algorithm)
-        print("🧠🧹",comm_manager.weights)
+        account_manager.login(user, password)
+        comm_manager = CommunicationManager(
+            account_manager.client, nodeConfig=config, routing_algorithm=routing_algorithm)
+        print("🧠🧹", comm_manager.weights)
         message_handler = MessageHandler(account_manager.client, comm_manager)
+
         if sendMessage:
-            message = {
-                'type': 'send_routing',
-                'to': 'val21240@alumchat.lol',
-                'from': username,
-                'data': 'Hello, this is a routing message.',
-                'hops': len(config.names['config'])
-            }
-            if routing_algorithm == 'flooding':
-                threading.Thread(target=comm_manager.sendRoutingMessageNeighbors, args=(message,)).start()
-            else:
-                threading.Thread(target=comm_manager.sendRoutingMessageDijkstra, args=(message,)).start()
+            recipient, user_input = ask_for_message()
+            if recipient and user_input:  # Verifica que no sean None
+                message = {
+                    'type': 'send_routing',
+                    'to': recipient,
+                    'from': username,
+                    'data': user_input,
+                    'hops': len(config.names['config'])
+                }
+                if routing_algorithm == 'flooding':
+                    threading.Thread(
+                        target=comm_manager.sendRoutingMessageNeighbors, args=(message,)).start()
+                else:
+                    threading.Thread(
+                        target=comm_manager.sendRoutingMessageDijkstra, args=(message,)).start()
+
         if comm_manager.routing_algorithm == 'flooding':
             await message_handler.receive_messages()
         else:
-            comm_manager.sendEcho()  
+            comm_manager.sendEcho()
             await message_handler.receive_messages()
     except Exception as e:
         print(f"Error: {e}")
@@ -46,18 +71,22 @@ async def startNode(node_id: str, password: str, routing_algorithm: str, type_na
         # Cerrar sesión
         account_manager.logout()
 
-def asyncNode(node_id: str, password: str, routing_algorithm: str, type_names: str, type_topo: str, sendMessage= False):
-    asyncio.run(startNode(node_id, password, routing_algorithm, type_names, type_topo, sendMessage))
+
+def asyncNode(node_id: str, password: str, routing_algorithm: str, type_names: str, type_topo: str, sendMessage=False):
+    asyncio.run(startNode(node_id, password, routing_algorithm,
+                type_names, type_topo, sendMessage))
 
 
 if __name__ == "__main__":
 
     if sys.argv[1] == 'true':
         threads = []
-        threads.append(threading.Thread(target=asyncNode, args=('B', 'ines130602', sys.argv[2], '../config/names2024-Conjunto1-9-4-2024.txt', '../config/topo2024-Conjunto1-9-4-2024.txt')))
-        threads.append(threading.Thread(target=asyncNode, args=('C', '123', sys.argv[2], '../config/names2024-Conjunto1-9-4-2024.txt', '../config/topo2024-Conjunto1-9-4-2024.txt')))
-        #threads.append(threading.Thread(target=asyncNode, args=('A', '1234', sys.argv[2], '../config/names2024-randomX-2024.txt', '../config/topo2024-randomX-2024.txt')))
-        #threads.append(threading.Thread(target=asyncNode, args=('E', '1234', sys.argv[2], '../config/names2024-randomX-2024.txt', '../config/topo2024-randomX-2024.txt')))
+        threads.append(threading.Thread(target=asyncNode, args=(
+            'B', 'ines130602', sys.argv[2], '../config/names2024-Conjunto1-9-4-2024.txt', '../config/topo2024-Conjunto1-9-4-2024.txt')))
+        threads.append(threading.Thread(target=asyncNode, args=(
+            'C', '123', sys.argv[2], '../config/names2024-Conjunto1-9-4-2024.txt', '../config/topo2024-Conjunto1-9-4-2024.txt')))
+        # threads.append(threading.Thread(target=asyncNode, args=('A', '1234', sys.argv[2], '../config/names2024-randomX-2024.txt', '../config/topo2024-randomX-2024.txt')))
+        # threads.append(threading.Thread(target=asyncNode, args=('E', '1234', sys.argv[2], '../config/names2024-randomX-2024.txt', '../config/topo2024-randomX-2024.txt')))
 
         for thread in threads:
             thread.start()
@@ -68,5 +97,6 @@ if __name__ == "__main__":
     else:
         node_id = sys.argv[1]
         password = sys.argv[2]
-        routing_algorithm = sys.argv[3]    
-        asyncio.run(startNode(node_id, password, routing_algorithm, '../config/names2024-Conjunto1-9-4-2024.txt', '../config/topo2024-Conjunto1-9-4-2024.txt', True))
+        routing_algorithm = sys.argv[3]
+        asyncio.run(startNode(node_id, password, routing_algorithm,
+                    '../config/names2024-Conjunto1-9-4-2024.txt', '../config/topo2024-Conjunto1-9-4-2024.txt', True))
